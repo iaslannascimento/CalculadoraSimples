8a7af7db0a956ced59d0a8330b1f4261fceda683
iaslannascimento <iaslan.nascimento@hotmail.com>
2018-12-18 12:40:49 -0300

    ok

--- app/src/main/java/com/example/user/calculadorasimples/MainActivity.java
+++ app/src/main/java/com/example/user/calculadorasimples/MainActivity.java
@@ -1,9 +1,20 @@
 package com.example.user.calculadorasimples;
-
+//projeto calculadora simples, os valores aparecem no campo text todos inclusive os sinais;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //campos do layout
+    //botões
+    Button botao1,botao2,botao3,botao4,botao5,botao6,botao7,botao8,botao9,botao0,mult,plus,menos,div,apagar,cancel;
+    //campo de texto
+    TextView valores;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
- 
+
 
     }
 }
